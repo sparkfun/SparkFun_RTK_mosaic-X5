fc0d44e794c68e987dca952ee9c2c6d08c15e41d
PaulZC <pc235603@gmail.com>
2024-08-19 08:24:41 +0100

    Add a local copy of esptool.exe 4.6

--- Firmware/RTK_mosaic-X5_Firmware/ESP32_FLASH_ERASE.bat
+++ Firmware/RTK_mosaic-X5_Firmware/ESP32_FLASH_ERASE.bat
@@ -15,8 +15,6 @@ for /f "tokens=2 delims=(" %%a in ('wmic path win32_pnpentity get caption /forma
 
 :erase
 
-::set ESPTOOL="C:\Users\public\Documents\GitHub\qcvault-live\UTILITIES\Espressif\esptool.exe"
-::set ESPTOOL="C:\Program Files (x86)\Arduino\hardware\espressif\esp32\tools\esptool\esptool.exe"
-set ESPTOOL="C:\Users\%USERNAME%\AppData\Local\Arduino15\packages\esp32\tools\esptool_py\4.5.1\esptool.exe"
+set ESPTOOL="esptool.exe"
 
 call %ESPTOOL% --chip esp32 -p %COMPORT% -b 460800 erase_flash
